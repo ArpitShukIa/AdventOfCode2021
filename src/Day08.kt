f7b694ac60fa5a67a3e4647f954f51b39f6425ec
arpit <arpitshukla62@gmail.com>
2021-12-08 19:38:48 +0530

    Update Day08

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,38 +1,29 @@
-private val String.missingChar get() = ('a'..'g').find { it !in this }!!
-private val String.missingChars get() = ('a'..'g').filter { it !in this }
-
 fun main() {
 
-    fun String.partition(): List<List<String>> {
-        return this.split("|").map {
-            it.trim().split(" ").map { segment ->
-                segment.toList().sorted().joinToString("")
-            }
-        }
-    }
+    fun String.partition() = split("|").map { it.trim().split(" ").map(String::toSet) }
 
     fun part1(input: List<String>): Int {
         return input.sumOf { line ->
-            line.partition()[1].count { it.length in listOf(2, 3, 4, 7) }
+            line.partition()[1].count { it.size in listOf(2, 3, 4, 7) }
         }
     }
 
     fun part2(input: List<String>): Int {
         return input.sumOf { line ->
             val (left, right) = line.partition()
-            val wordsOfLen = List(8) { len -> left.filter { it.length == len } }
-            val segment = Array(10) { "" }
-            segment[1] = wordsOfLen[2].single()
-            segment[4] = wordsOfLen[4].single()
-            segment[7] = wordsOfLen[3].single()
-            segment[8] = wordsOfLen[7].single()
-            segment[6] = wordsOfLen[6].find { it.missingChar in segment[1] }!!
-            segment[9] = wordsOfLen[6].find { it.missingChar !in segment[4] }!!
-            segment[0] = wordsOfLen[6].find { it != segment[6] && it != segment[9] }!!
-            segment[2] = wordsOfLen[5].find { s -> s.missingChars.all { it in segment[4] } }!!
-            segment[3] = wordsOfLen[5].find { s -> s.missingChars.none { it in segment[1] } }!!
-            segment[5] = wordsOfLen[5].find { it != segment[2] && it != segment[3] }!!
-            right.map { segment.indexOf(it) }.joinToString("").toInt()
+            val wordsOfLen = List(8) { len -> left.filter { it.size == len } }
+            val arr = Array(10) { setOf<Char>() }
+            arr[1] = wordsOfLen[2].first()
+            arr[4] = wordsOfLen[4].first()
+            arr[7] = wordsOfLen[3].first()
+            arr[8] = wordsOfLen[7].first()
+            arr[6] = wordsOfLen[6].first { it + arr[1] == arr[8] }
+            arr[5] = wordsOfLen[5].first { it + arr[6] != arr[8] }
+            arr[2] = wordsOfLen[5].first { it + arr[5] == arr[8] }
+            arr[9] = wordsOfLen[6].first { it + arr[4] != arr[8] }
+            arr[0] = wordsOfLen[6].first { it + arr[5] == arr[8] }
+            arr[3] = wordsOfLen[5].first { it !in arr }
+            right.map { arr.indexOf(it) }.joinToString("").toInt()
         }
     }
 
